9daec94f9be0c5971152202bd64d0bcd35f1258c
darkfolcer <bilalkutlu@live.com>
2016-12-01 15:00:21 +0200

    initilazing settings of project

--- Project/online-exam/online-exam/Share.Master.cs
+++ Project/online-exam/online-exam/Share.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace online_exam
 {
-    public partial class WebForm3 : System.Web.UI.Page
+    public partial class Share : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- Project/online-exam/online-exam/Share.Master.designer.cs
+++ Project/online-exam/online-exam/Share.Master.designer.cs
@@ -11,7 +11,7 @@ namespace online_exam
 {
 
 
-    public partial class WebForm2
+    public partial class Share
     {
 
         /// <summary>
@@ -22,5 +22,14 @@ namespace online_exam
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }

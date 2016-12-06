6d64cb063c4010802c5790cbe59727d67a03b2f9
darkfolcer <bilalkutlu@live.com>
2016-12-06 19:14:22 +0200

    login page is now works like a charm for any kind of user.

--- Project/online-exam/online-exam/login.aspx.cs
+++ Project/online-exam/online-exam/login.aspx.cs
@@ -9,9 +9,25 @@ namespace EsOnlineExam
 {
     public partial class login : System.Web.UI.Page
     {
+        Database db = new Database();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void loginBtn_Click(object sender, EventArgs e)
+        {
+            int result = db.UserLogin(userNameTxt.Text.ToString(), passwordTxt.Text.ToString());
+            if (result == 1)
+                Response.Redirect("~/student/index.aspx");
+            else if (result == 2)
+                Response.Redirect("~/instructor/index.aspx");
+            else
+            {
+                lbl_Failed.Text = "Username or password is wrong. Please check your details.";
+                lbl_Failed.Visible = true;
+
+            }
         }
     }
 }
